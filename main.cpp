9086e3d3c337c6fb1e3a1d6b9764096ad2435eb0
GitUser <GitUser@mail.com>
2021-01-08 12:36:45 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include<unordered_set>
+
+///N'arrivant pas à télécharger la liste j'ai créer un fichier "words.txt" avec trois mots: bonjour, aurevoir, revoir
+
+int main()
+{
+    std::fstream dictionnaire;
+    std::string line, mot;
+    std::unordered_set<std::string> table;
+
+    dictionnaire.open("words.txt");
+    if (dictionnaire.is_open())
+    {
+        while (getline(dictionnaire,line))
+        {
+          std::cout<<line<<std::endl;
+          table.insert(line);
+        }
+
+        dictionnaire.close();
+    }
+    else{
+        std::cout<<"dico non trouve"<<std::endl;
+    }
+    std::cout<<"saisir mot"<<std::endl;
+    std::cin>>mot;
+
+    if (table.find (mot)==table.end())
+    {
+        std::cout<<"mot pas trouve"<<std::endl;
+    }
+    else{
+        std::cout<<"mot trouve"<<std::endl;
+    }
+    return 0;
+}
